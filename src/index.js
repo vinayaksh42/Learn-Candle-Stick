9b9749b20ccab2043e50f072f4504197bbac6161
Priyanshu <priyanshusharma2601@gmail.com>
2022-03-31 22:17:39 +0530

    router commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { BrowserRouter } from 'react-router-dom'; 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,9 +6,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+    <BrowserRouter>
     <App />
-  </React.StrictMode>,
+    </BrowserRouter>,
+  
   document.getElementById('root')
 );
 
