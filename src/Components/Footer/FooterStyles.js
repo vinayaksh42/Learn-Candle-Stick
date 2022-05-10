15a496b95eaaaed3bd87451a5332a9600d592a64
vinayaksh42 <vinayaksh42@gmail.com>
2022-05-10 20:00:32 +0530

    error fix

--- src/Components/Footer/FooterStyles.js
+++ src/Components/Footer/FooterStyles.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 export const Box = styled.div`
-padding: 80px 60px;
+padding: 80px 0px;
 background: black;
 position: absolute;
 bottom: 0;
 width: 100%;
-
+overflowX: hidden;
 
 @media (max-width: 1000px) {
 	padding: 70px 30px;
