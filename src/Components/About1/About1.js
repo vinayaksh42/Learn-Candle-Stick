520a8420999411280c4b8815b9f4465d23b5cfee
vinayaksh42 <vinayaksh42@gmail.com>
2022-05-11 23:14:13 +0530

    fix

--- src/Components/About1/About1.js
+++ src/Components/About1/About1.js
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
