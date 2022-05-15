9d71333943384fe1bb2454dedeb622ce3a6aa8fe
Siddhant-vardhansingh <19bec088@smvdu.ac.in>
2022-05-15 14:40:02 +0530

    Made_quiz_page

---
+++ src/Pages/Quiz.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Quiz_container from '../Components/Quiz_container/Quiz_container';
+function Quiz(){
+    return(
+    <>
+        <Quiz_container/>
+    </>
+    )
+}
+export default Quiz;
+
