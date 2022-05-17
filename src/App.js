9b04f4a51f215458421d99d86d29029b42338e73
Harsh chaudhary <19bec029@smvdu.ac.in>
2022-05-17 15:07:41 +0530

    LearnModule

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './Components/Navbar/Navbar';
 import './App.css';
 import React from 'react';
 import Footer from './Components/Footer/Footer';
+import Learn from './Pages/Learn_Module';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="Quiz" element={<Quiz/>}/>
+        <Route path="LearnModule" element={<Learn/>}/>
       </Routes>
       <Footer/>
     </div>
